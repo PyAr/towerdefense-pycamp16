88972bf768b00101940d823cdd8c8a414fefcf4b
Sofia Denner <sofi.denner@gmail.com>
2016-03-25 01:34:44 -0300

    adding new kind of towers

--- core/towers.py
+++ core/towers.py
@@ -1,27 +1,34 @@
-#towers_dic={"Comunacha","Indecisa","Bullying","Fresquete","Zika","Patovica","ElNiño","Troll","Camper"}
-"""
-def get_kinds
-    return ListaDeTorres=["Enum"]
-"""
+import random
+
+
+def get_kinds():
+    return list(towers_dic.keys())
+
 
 def get_tower(position, kind):
-    return Tower(position)
+    klass = towers_dic[kind]
+    return klass(position)
+
 
 class Tower:
+    """Basic tower."""
 
     def __init__(self, position, shooting_range=30, strength=200, cooldown=4):
-        self.shooting_range = rango
-        self.strength= fuerza
-        self.position = posicion
-        self.cooldown=cooldown
+        self.shooting_range = shooting_range
+        self.strength = strength
+        self.position = position
+        self.cooldown = cooldown
         self.current_cooldown = 0
 
     def shoot(self, monsters):
         if len(monsters)==0:
             return
-        #monsters is a tuple's list (monster, distance)
-        monsters = sorted(monsters, key=lambda x: x[1])
-        monsters[0].affect(damage=self.strength)
+
+        targets = self._select_targets(monsters)
+        # shoot
+        for t in targets:
+            self._damage(t)
+
         self.current_cooldown = self.cooldown
 
     def pre_shoot(self, cooldown):
@@ -31,110 +38,63 @@ class Tower:
             self.current_cooldown -= 1
             return False
 
-    """
+    def _select_targets(self, monsters):
+        """
+        return a list with the nearest target
+        """
+        # monsters is a tuple's list (monster, distance)
+        # select target
+        monsters = sorted(monsters, key=lambda x: x[1])
+        return [monsters[0][0]]
 
-class Comunacha(tower):
+    def _damage(self, monster):
+        monster.affect(damage=self.strength)
 
-class Veneno(tower):
-    def __init__(self):
-    fuerza = 0
-    shooting_range = 0
-    cooldown = 0
-    def shoot(bichos):
-        if self.canShoot():
-            #a quien disparo
-            #despues aplico hit y affect
-        else:
-            pass
 
-class Veneno(tower):
-    fuerza = 0
-    shooting_range = 0
-    cooldown = 0
-    def shoot(bichos):
-        if self.canShoot():
-            #a quien disparo
-            #despues aplico hit y affect
-        else:
-            pass
+class Indecisa(Tower):
+    def _select_target(self, monsters):
+        """
+        return a list of targets by sampling
+        """
+        targets = [random.choice(monsters)]
+        monsters.remove(target1)
+        if monsters:
+            targets.append(random.choice(monsters))
+        return targets
 
-class Veneno(tower):
-    fuerza = 0
-    shooting_range = 0
-    cooldown = 0
-    def shoot(bichos):
-        if self.canShoot():
-            #a quien disparo
-            #despues aplico hit y affect
-        else:
-            pass
 
-class Veneno(tower):
-    fuerza = 0
-    shooting_range = 0
-    cooldown = 0
-    def shoot(bichos):
-        if self.canShoot():
-            #a quien disparo
-            #despues aplico hit y affect
-        else:
-            pass
+class Bully(Tower):
+    def __init__(self, position, shooting_range=20, strength=400, cooldown=2):
+        super().__init__(position, shooting_range=shooting_range, strength=strength,
+                         cooldown=cooldown)
 
-class Veneno(tower):
-    fuerza = 0
-    shooting_range = 0
-    cooldown = 0
-    def shoot(bichos):
-        if self.canShoot():
-            #a quien disparo
-            #despues aplico hit y affect
-        else:
-            pass
 
-class Veneno(tower):
-    fuerza = 0
-    shooting_range = 0
-    cooldown = 0
-    def shoot(bichos):
-        if self.canShoot():
-            #a quien disparo
-            #despues aplico hit y affect
-        else:
-            pass
+class Chiflete(Tower):
+    def __init__(self, position, shooting_range=30, strength=20, cooldown=4):
+        super().__init__(position, shooting_range=shooting_range, strength=strength,
+                         cooldown=cooldown)
 
-class Veneno(tower):
-    def __init__(self):
-        fuerza = 0
-        shooting_range = 0
-        cooldown = 0
-    def shoot(bichos):
-        if self.canShoot():
-            #a quien disparo
-            #despues aplico hit y affect
-        else:
-            pass
+    def _damage(self, monster):
+        monster.affect(damage=self.strength, freeze=5)
 
-class Veneno(tower):
-    def __init__(self):
-        fuerza = 0
-        shooting_range = 0
-        cooldown = 0
-    def shoot(bichos):
-        if self.canShoot():
-            #a quien disparo
-            #despues aplico hit y affect
-        else:
-            pass
 
-class Veneno(tower):
-    def __init__(self):
-        fuerza = 0
-        shooting_range = 0
-        cooldown = 0
-    def shoot(bichos):
-        if self.canShoot():
-            #a quien disparo
-            #despues aplico hit y affect
-        else:
-            pass
-"""
+class Fresquete(Tower):
+    def __init__(self, position, shooting_range=20, strength=400, cooldown=2):
+        super().__init__(position, shooting_range=shooting_range, strength=strength,
+                         cooldown=cooldown)
+
+
+class Zika(Tower):
+    def __init__(self, position, shooting_range=20, strength=400, cooldown=2):
+        super().__init__(position, shooting_range=shooting_range, strength=strength,
+                         cooldown=cooldown)
+
+
+towers_dic = {
+    "Comunacha": Tower,
+    "Indecisa": Indecisa,
+    "Bully": Bully,
+    "Chiflete": Chiflete,
+    "Fresquete": Fresquete,
+    "Zika": Zika
+}
