5eb09b8f8ca8824d6557ba55000eb0f4c386838b
Juan Pedro Fisanotti <fisadev@gmail.com>
2016-03-25 15:22:49 -0300

    Ability to save generations history

--- ai/genetic_loop.py
+++ ai/genetic_loop.py
@@ -1,6 +1,7 @@
 from weighted_random import weighted_random_values
 from core import get_available_locations, get_tower_types, start
 import random
+import json
 
 
 def add_game_values(games):
@@ -40,7 +41,10 @@ class Genetic:
         return child1, child2
 
     def loop(self, initial_games, mutation_factor=0.1, n_games=10, cut_value=0,
-             max_iterations=10000):
+             max_iterations=10000, save_generations_to=None):
+
+        if save_generations_to:
+            self.prepare_generations_file(save_generations_to)
 
         if n_games % 2 != 0:
             raise Exception("n_games tiene que ser par")
@@ -48,6 +52,8 @@ class Genetic:
         iterations = 0
         current_games = add_game_values(initial_games)
         while True:
+            if save_generations_to:
+                self.save_generation(current_games)
 
             if iterations == max_iterations:
                 return current_games
@@ -63,6 +69,13 @@ class Genetic:
 
             current_games = add_game_values(childs)
             iterations += 1
+
+    def prepare_generations_file(self, generations_file_name):
+        self.generations_file = open(generations_file_name, 'w')
+
+    def save_generation(self, games):
+        self.generations_file.write(json.dumps(games))
+        self.generations_file.flush()
 
 
 if __name__ == '__main__':
